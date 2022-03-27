24c507ce7bd881d870f12edbc29cff76aa723995
Bartosz Sulima <barul97@gmail.com>
2022-03-27 17:42:40 +0200

    Chenged distance matrix calculations to player volume list calculations

--- minecraft-plugin/src/main/java/com/pvchat/proximityvoicechat/plugin/ProximityVoiceChat.java
+++ minecraft-plugin/src/main/java/com/pvchat/proximityvoicechat/plugin/ProximityVoiceChat.java
@@ -1,11 +1,14 @@
 package com.pvchat.proximityvoicechat.plugin;
 
-import com.pvchat.proximityvoicechat.plugin.distanceMatrix.PlayerDistanceMatrix;
+import com.pvchat.proximityvoicechat.plugin.distanceMatrix.PlayerDistanceAndVolumeCalculations;
+import com.pvchat.proximityvoicechat.plugin.distanceMatrix.PlayerVolumeData;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
 
 public final class ProximityVoiceChat extends JavaPlugin {
     private ConfigManager configManager;
-    private PlayerDistanceMatrix playerDistanceMatrix;
+    private PlayerDistanceAndVolumeCalculations playerDistanceAndVolumeCalculations;
 
     @Override
     public void onEnable() {
@@ -16,9 +19,19 @@ public final class ProximityVoiceChat extends JavaPlugin {
         //Load config from config.yml file
         configManager.loadConfig();
 
-        playerDistanceMatrix = new PlayerDistanceMatrix(this);
+        playerDistanceAndVolumeCalculations = new PlayerDistanceAndVolumeCalculations(this);
 
-        playerDistanceMatrix.updateMatrix();
+        playerDistanceAndVolumeCalculations.updatePlayerList();
+
+        ArrayList<PlayerVolumeData> volumeList=playerDistanceAndVolumeCalculations.playerVolumeList();
+        if (volumeList!=null){
+            for (int i = 0; i < volumeList.size(); i++) {
+                PlayerVolumeData temp=volumeList.get(i);
+                System.out.println("Player1: "+temp.getPlayer1ID()+" Player2: "+temp.getPlayer2ID()+" Volume: "+temp.getVolumeLevel());
+
+            }
+        }
+
     }
 
     @Override
