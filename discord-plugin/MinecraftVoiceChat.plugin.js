b053c5f2140641de08c0a0627d94fe4810d9588e
smachay_249699 <smachay_249699@e-science.pl>
2022-05-01 21:52:18 +0200

    Added restoring user local volumes from before they entered the channel

--- discord-plugin/MinecraftVoiceChat.plugin.js
+++ discord-plugin/MinecraftVoiceChat.plugin.js
@@ -11,6 +11,7 @@ const userId = BdApi.findModuleByProps("getCurrentUser").getCurrentUser().id;
 
 let channelPVC = "949028178599510061";
 let socket;
+let previousVolumes;
 
 const join = (filters) => {
   const apply = filters.filter((filter) => filter instanceof Function);
@@ -32,14 +33,42 @@ const find = (...filters) => raw.single(join(filters));
 const byProps = (...props) => find(byProps$1(props));
 
 const SettingsActions = byProps("setLocalVolume");
+const getSettingsActions = byProps("getLocalVolume");
 const AudioConvert = byProps("perceptualToAmplitude");
 
 const setVolume = (userId, value) => {
+  setPreviousVolumes(userId);
   SettingsActions.setLocalVolume(
     userId,
     AudioConvert.perceptualToAmplitude(value),
     "default"
   );
+};
+
+const getVolume = (userId) => {
+  const volume = getSettingsActions.getLocalVolume(userId, "default");
+  return AudioConvert.amplitudeToPerceptual(volume);
+};
+
+//checks if user's initial volume value was saved
+const setPreviousVolumes = (userId) => {
+  if (previousVolumes === null) {
+    previousVolumes = [];
+    previousVolumes.push({ user: userId, volume: getVolume(userId) });
+  } else if (!previousVolumes.find(({ user }) => user === userId)) {
+    previousVolumes.push({ user: userId, volume: getVolume(userId) });
+  }
+};
+
+//restores user local volumes from before they entered the channel
+const restorePreviousVolumes = () => {
+  if (previousVolumes !== null) {
+    for (const { user, volume } of previousVolumes) {
+      setTimeout(() => {
+        setVolume(user, volume);
+      }, 1000);
+    }
+  }
 };
 
 const updateUserVolumes = (userVolumes) => {
@@ -57,8 +86,7 @@ const checkChannel = () => {
     "getLastSelectedChannelId"
   ).getVoiceChannelId();
 
-  if (currId === channelPVC && socket === null) {
-    //IP: ws://localhost:8080?discordID=0
+  if (currId === channelPVC) {
     socket = new WebSocket(`ws://localhost:8080?discordID=${userId}`);
     socket.onopen = function (e) {
       socket.send("Connected");
@@ -66,16 +94,18 @@ const checkChannel = () => {
     socket.onmessage = function (event) {
       updateUserVolumes(event.data);
     };
-  } else {
-    if (socket != null) {
-      socket = null;
-    }
+  } else if (socket !== null) {
+    socket.close(1000);
+    socket = null;
+    restorePreviousVolumes();
+    previousVolumes = null;
   }
 };
 
 module.exports = class MinecraftVoiceChat {
   load() {
     socket = null;
+    previousVolumes = null;
     checkChannel();
   }
 
